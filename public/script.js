ed0f7d104d359f5804e14df142e7954d4de0af41
Mithun Halder <mithunrptc@gmail.com>
2019-04-10 00:38:35 +0600

    Finishing the project

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 Vue.config.devtools = true;
+var LOAD_NUM = 10;
 var app = new Vue({
     el: '#app',
     data:{
@@ -8,20 +9,36 @@ var app = new Vue({
         cart:[],
         search:'anime',
         lastSearch:'',
-        loading:false
+        loading:false,
+        results: []
+    },
+    computed:{
+        noMoreItems(){
+         return   this.items.length === this.results.length && this.results.length > 0 ;
+        }
     },
     methods:{
+        appendItems(){
+            if(this.items.length < this.results.length){
+                let append = this.results.slice(this.items.length, this.items.length + LOAD_NUM);
+                this.items = this.items.concat(append);
+            }
+        },
         onSubmit(){
-            this.items = [];
-            this.loading = true;
-           this.$http
-           .get('/search/'.concat(this.search))
-            .then(function(res){
-                this.items = res.data;
-                this.loading = false;
-                this.lastSearch= this.search
-
-            })
+            if(this.search.length){            
+                this.items = [];
+                this.loading = true;
+                this.$http
+                .get('/search/'.concat(this.search))
+                .then(function(res){
+                    this.lastSearch= this.search
+                    this.results = res.data;
+                    this.appendItems()
+                    this.loading = false;
+                })
+            }else{
+                alert("Search cannot be empty")
+            }
         },
         addItem(index){
             this.total+=9.99;
@@ -67,5 +84,13 @@ var app = new Vue({
     },
     mounted(){
         this.onSubmit()
+
+        var vueInstance = this;
+        var elem = document.getElementById('product-list-bottom');
+        var watcher = scrollMonitor.create(elem);
+        watcher.enterViewport(function(){
+            vueInstance.appendItems()
+        })
     }
 });
+
